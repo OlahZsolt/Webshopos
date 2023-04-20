edbe273dfea96647ed545932dd7d3a8e2d9ca231
Vicziand <113669347+Vicziand@users.noreply.github.com>
2023-04-20 20:09:42 +0200

    regisztráció adatbázis

--- php/connect.php
+++ php/connect.php
@@ -7,7 +7,14 @@
  $address=$_POST['address'];
  $address2=$_POST['address2'];
  $city=$_POST['city'];
+ $state=$_POST['state'];
+ $zip=$_POST['zip'];
 
+ $deliveryAddress=$_POST['deliveryAddress'];
+ $deliveryAddress2=$_POST['deliveryAddress2'];
+ $deliveryCity=$_POST['deliveryCity'];
+ $deliverySate=$_POST['deliveryState'];
+ $deliveryZip=$_POST['deliveryZip'];
 
 //adatbázis kapcsolás
 
@@ -16,9 +23,9 @@ if($conn->connect_error){
     die('Megszakadt a kapcsolat : '.$conn->connect_error);
 }
 else{
-    $stmt= $conn->prepare("insert into registration(email,password,lastName,firstName,phone,address,address2,city)
-    values(?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("ssssisss",$email,$password,$lastName,$firstName,$phone,$address,$address2,$city);
+    $stmt= $conn->prepare("insert into registration(email,password,lastName,firstName,phone,address,address2,city,state,zip,deliveryAddress,deliveryAddress2,deliveryCity,deliveryState,deliveryZip)
+    values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssissssssssss",$email,$password,$lastName,$firstName,$phone,$address,$address2,$city,$state,$zip,$deliveryAddress,$deliveryAddress2,$deliveryCity,$deliveryState,$deliveryZip);
     $stmt->execute();
     echo "Sikeres regisztráció...";
     $stmt->close();
