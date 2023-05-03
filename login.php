babd7618ef3be0752d8a208ce3c9f7e2e41fdf32
Vicziand <113669347+Vicziand@users.noreply.github.com>
2023-05-03 21:21:06 +0200

    login update

--- login.php
+++ login.php
@@ -2,7 +2,24 @@
 $email= $_POST['email'];
 $password= $_POST['password'];
 
-$conn= new mysqli('localhost','root','','webshop_users');
+$con= new mysqli('localhost','root','','webshop_users');
+if($con -> connect_error){
+    die("Megszakadt a kapcsolat: ".$con->connect_error);
+}else{
+    $stmt = $con -> prepare("SELECT * FROM registration WHERE email = ?");
+    $stmt -> bind_param("s",$email);
+    $stmt -> execute();
+    $stmt_result = $stmt -> get_result();
+    if($stmt_result -> num_rows > 0 ){
+        $data= $stmt_result -> fetch_assoc();
+        if($data['password'] === $password){
+            echo "<h2>Sikeres bejelentkezés</h2>";
+        }
+    }
+    else{
+        echo "<h2>Nem megfelelő az email vagy a jelszó</h2>";
+    }
+}
 
 
 ?>
